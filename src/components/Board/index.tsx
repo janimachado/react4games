db8272fdcc51d1c799160e4dcb700be8b3a7db05
janimachado <janicota@hotmail.com>
2021-03-03 19:26:33 -0300

    intro 2 aula

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { GAME_SIZE } from '../../settings/constants';
 import Hero from '../Hero';
+import MiniDemon from '../MiniDemon';
+
 
 const Board = () => {
     return (
         <div>
+            <MiniDemon />
             <Hero />
             <img src="./assets/tileset.gif" width={GAME_SIZE} height={GAME_SIZE}></img>
         </div>
--- src/components/MiniDemon/index.tsx
+++ src/components/MiniDemon/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { TILE_SIZE } from '../../settings/constants';
 import './index.css';
 
-const Hero = () => {
+const MiniDemon = () => {
     return (
         <div style={{
             backgroundRepeat: 'no-repeat',
             width: TILE_SIZE,
             height: "100px",
-            backgroundImage: "url(./assets/HERO.png)",
-            animation: 'hero-animation 0.5s steps(4) infinite',
+            backgroundImage: "url(./assets/MINI-DEMON.png)",
+            animation: 'mini-demon-animation 0.5s steps(4) infinite',
             position: 'absolute',
-            bottom: TILE_SIZE * 3,
-            left: TILE_SIZE * 3,
+            bottom: TILE_SIZE * 1,
+            left: TILE_SIZE * 1,
         }}></div>
     );
 }
 
-export default Hero;
+export default MiniDemon;
